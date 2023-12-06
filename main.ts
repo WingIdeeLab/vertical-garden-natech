da3c78e84c50a4ca26d6cd9e0c80d425d0345c98
WingIdeeLab <94964914+WingIdeeLab@users.noreply.github.com>
2023-12-06 11:19:30 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+let _4Digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+_4Digit.set(7)
+basic.forever(function () {
+    images.iconImage(IconNames.SmallHeart).showImage(0)
+    control.waitMicros(250000)
+    images.iconImage(IconNames.Heart).showImage(0)
+    _4Digit.show(pins.analogReadPin(AnalogPin.P2))
+    control.waitMicros(250000)
+    if (pins.analogReadPin(AnalogPin.P2) < 150) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
