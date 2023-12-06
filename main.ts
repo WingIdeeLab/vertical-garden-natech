84419f0831d3d57d1056bab6ecff7f9e5c7f9172
WingIdeeLab <94964914+WingIdeeLab@users.noreply.github.com>
2023-12-06 11:39:26 +0100

    Ein aus

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 let _4Digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 _4Digit.set(7)
 basic.forever(function () {
-    images.iconImage(IconNames.SmallHeart).showImage(0)
-    control.waitMicros(250000)
-    images.iconImage(IconNames.Heart).showImage(0)
-    _4Digit.show(pins.analogReadPin(AnalogPin.P2))
-    control.waitMicros(250000)
-    if (pins.analogReadPin(AnalogPin.P2) < 150) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
+    if (input.pinIsPressed(TouchPin.P3)) {
+        images.iconImage(IconNames.SmallHeart).showImage(0)
+        control.waitMicros(250000)
+        images.iconImage(IconNames.Heart).showImage(0)
+        _4Digit.show(pins.analogReadPin(AnalogPin.P2))
+        control.waitMicros(250000)
+        if (pins.analogReadPin(AnalogPin.P2) < 150) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        } else {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        }
     } else {
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        images.iconImage(IconNames.No).showImage(0)
     }
 })
